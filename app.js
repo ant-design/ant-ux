515764449751d31c2d1ad57cb2bda86b33032d88
CaiCai <hi@cai-cai.me>
2016-01-13 16:18:42 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+/*
+ * Module dependencies
+ */
+var express = require('express');
+
+var app = express();
+var options = {
+  dotfiles: 'ignore',
+  etag: false,
+  extensions: ['htm', 'html'],
+  index: false,
+  maxAge: '1d',
+  redirect: false,
+  setHeaders: function (res, path, stat) {
+    res.set('x-timestamp', Date.now());
+  }
+}
+
+app.set('view engine', 'jade');
+app.use(express.static('./', options));
+
+app.get('/', function (req, res) {
+  res.render('index');
+});
+app.listen(3000);
+
---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+var gulp = require('gulp');
+var watch = require('gulp-watch');
+var jade = require('gulp-jade');
+var cssmin = require('gulp-cssmin');
+
+gulp.task('jade', function() {
+	gulp.src(['./views/*.jade'])
+		.pipe(jade({
+			pretty: true
+		}))
+		.pipe(gulp.dest('./'));
+});
+
+gulp.task('css', function () {
+	gulp.src('views/public/css/*.css')
+		.pipe(cssmin())
+		.pipe(gulp.dest('./public/css'));
+});
+
+gulp.task('js', function() {
+	gulp.src('views/public/js/*.js')
+	.pipe(gulp.dest('./public/js/'));
+});
+
+gulp.task('site', ['jade','css','js']);
